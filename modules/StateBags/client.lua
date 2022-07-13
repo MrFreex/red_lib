d284326943612aabc986439d4b5ebc54340696ae
MrFreex <filippolissandrin@gmail.com>
2022-07-13 15:19:38 +0200

    StateBags Hooks

--- modules/StateBags/client.lua
+++ modules/StateBags/client.lua
@@ -6,9 +6,35 @@ local Bags = {
     Global = {}
 }
 
+local BagsCallback = {}
+
+local function callThem(stateIndex, newValue, object, table)
+    for k,v in pairs(table) do
+        v(stateIndex,newValue,object)
+    end
+end
+
+local function checkForCallbacks(object, stateIndex, newValue) -- Checks for Hooks and calls the supplied functions
+    if BagsCallback[object.__type] then -- Is there any hook for the bag type?
+        local BagsOfType = BagsCallback[object.__type]
+        callThem(BagsOfType.global) -- Call the global ones 
+
+        if BagsOfType.bags[object.id] then -- Is there any hook for this bag?
+            local ForSingleBag = BagsOfType.bags[object.id]
+
+            callThem(ForSingleBag.global) do -- Call the global ones
+
+            if ForSingleBag.indexes[stateIndex] then
+                callThem(ForSingleBag.indexes[stateIndex]) -- Call the index-bound callbacks
+            end
+        end
+    end
+end
+
 
 local Sync = {
     server = function(bag, index, newv)
+        checkForCallbacks(bag,index,newv)
         Events.TriggerServer("syncBag", { bag,index,newv })
     end,
 
@@ -20,6 +46,43 @@ local Sync = {
         Bags?[type(bag)][bag.id]?.state[k] = v
     end
 }
+
+exports("HookBag", function(bagType, id, k, cb)
+    if not bagType then return end
+    BagsCallback[bagType] = BagsCallback[bagType] or {
+        bags = {},
+        global = {}
+    }
+
+    if not cb then
+        if type(k) == "function" then
+            cb = k
+        else -- Return cause no callback was supplied
+            return false
+        end
+    end
+
+    if id then
+        BagsCallback[bagType].bags[id] = BagsCallback[bagType].bags[id] or {
+            global = {},
+            indexes = {}
+        }
+    else
+        table.insert(BagsCallback[bagType].global, cb)
+        return true
+    end
+
+    local ref = BagsCallback[bagType].bags[id]
+
+    if type(k) ~= "function" then
+        ref.indexes[k] = ref.indexes[k] or {}
+        table.insert(ref.indexes[k], cb)
+    else
+        table.insert(ref.global, cb)
+    end
+
+    return true
+end)
 
 Events.Register("syncBag", Sync.me)
 
