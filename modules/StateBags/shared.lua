58a422f7385756ebfb8ec938801c43d928ce01d8
MrFreex <33759569+MrFreex@users.noreply.github.com>
2022-07-13 16:42:54 +0200

    feat: UnHook state bags

--- modules/StateBags/shared.lua
+++ modules/StateBags/shared.lua
@@ -1,9 +1,16 @@
 BagsCallback = {}
 
+local lastId = -1
+
+function returnAndIncrement()
+    lastId = lastId + 1
+    return lastId
+end
+
 exports("HookBag", function(bagType, id, k, cb)
     if not bagType then return end
 
-    local hookId
+    
     local res = GetInvokingResource()
 
     BagsCallback[bagType] = BagsCallback[bagType] or {
@@ -19,44 +26,46 @@ exports("HookBag", function(bagType, id, k, cb)
         end
     end
 
+    local hookId = returnAndIncrement()
+
     if id then
         BagsCallback[bagType].bags[id] = BagsCallback[bagType].bags[id] or {
             global = {},
             indexes = {}
         }
     else
-        table.insert(BagsCallback[bagType].global, {cb,res})
-        return { bagType, "global", #BagsCallback[bagType].global }
+        table.insert(BagsCallback[bagType].global, {cb,res,hookId})
+        return hookId
     end
 
     local ref = BagsCallback[bagType].bags[id]
 
     if _TYPE(k) == "string" then
         ref.indexes[k] = ref.indexes[k] or {}
-        table.insert(ref.indexes[k], {cb,res})
-        return { bagType, "bags", id, "indexes", k, #ref.indexes[k] }
+        table.insert(ref.indexes[k], {cb,res,hookId})
+        return hookId
     else
-        table.insert(ref.global, {cb,res})
-        return { bagType, "bags", id, "global", #ref.global }
+        table.insert(ref.global, {cb,res,hookId})
+        return hookId
     end
+
+    hookId = hookId - 1 --// This ID was not used, so it is best to decrement it
 end)
 
-exports("UnHookBag", function(hookId))
+
 
 local function callThem(stateIndex, newValue, object, table)
-    for k,v in pairs(table) do print(k, v[1], v[2]) end
     for k,v in pairs(table) do
-        v[1](stateIndex,newValue,object)
+        v[1](stateIndex,newValue,object,v[3])
     end
 end
 
-local function cleanup(tab, res)
-    
+local function cleanup(tab, res, index)
+    local index = index or 2
     local toRem = {}
 
     for k,e in pairs(tab) do
-        print(e[2], res)
-        if e[2] == res then
+        if e[index] == res then
             table.insert(toRem,k)
         end
     end
@@ -64,19 +73,35 @@ local function cleanup(tab, res)
     for i=1,#toRem do
         table.remove(tab, toRem[i])
     end
+
+    return #toRem > 0
 end
 
-AddEventHandler("onResourceStop", function(res)
-    print("[RED] Cleaning Statebags for ", res)
+--[[
+    Cleans all callbacks given a search query
+    @param: res, what to search for
+    @param: index, the index where @res is located
+    @param: qf, quit at first match
+]]
+local function cleanAll(res,index,qf)
     for k,e in pairs(BagsCallback) do
-        cleanup(e.global,res)
+        if cleanup(e.global,res, index) and qf then return end
         for i,v in pairs(e.bags) do
-            cleanup(v.global,res)
+            if cleanup(v.global,res, index) and qf then return end
             for j,w in pairs(v.indexes) do
-                cleanup(w, res)
+                if cleanup(w, res, index) and qf then return end
             end
         end
     end
+end
+
+exports("UnHookBag", function(hookId)
+    cleanAll(hookId, 3, true)
+end)
+
+AddEventHandler("onResourceStop", function(res)
+    print("[RED] Cleaning Statebags for ", res)
+    cleanAll(res,2)
 end)
 
 function checkForCallbacks(object, stateIndex, newValue) -- Checks for Hooks and calls the supplied functions
